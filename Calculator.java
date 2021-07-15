e31bddc52bb18ed08f52c2ffed468f798a5ec4d6
T Bhargavi <t.bhargavi@ibm.com>
2021-07-15 12:53:49 +0530

    First time Calculator.java added to repo

---
+++ Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator{
+public static int square(int x)
+{
+return x*x;
+}
+public ststic int sum(int x,int y)
+{
+return x+y;
+}
+public static int power(int x,int n)
+{
+int res=1;
+for(;n>0;n--)
+{
+res=res*x;
+return res;
+}
+}
+}
