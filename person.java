403b398c1015a42cc92fb771f034bcc5af6312b3
T Bhargavi <t.bhargavi@ibm.com>
2021-07-15 16:44:42 +0530

    files added

---
+++ person.java
@@ -0,0 +1,19 @@
+public class person
+{
+private int ssn;
+private String name;
+private int age;
+public person()
+{
+}
+public person(int ssn,String name,int age)
+{
+this.ssn=ssn;
+this.name=name;
+this.age=age;
+}
+public static void main(String[] args)
+{
+person p1=new person(123,bhargavi,22)
+}
+}
